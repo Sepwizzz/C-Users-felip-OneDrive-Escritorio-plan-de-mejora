6f35272dbc9e34c20c1bf83de938bd85bc9b1b90
Sepwizzz <115958805+Sepwizzz@users.noreply.github.com>
2022-11-24 21:25:42 -0500

    Update buc1.py
    pongo libro

--- bucles/buc1.py
+++ bucles/buc1.py
@@ -1,6 +1,7 @@
+#pagina: 119 libro : python 3: curso parctico
+#capitulo 3. elementos fundamentales de programacion
+
 i = 0
-
-
 for i in range(11, 22, 5): #pocision 1= numero inicial si no se pone empesara en 0 posision 2= el numero final o donde quieres que ternmine pocision 3= de cuanto en cuanto va a acontar 
     print(i) #tambien se puede hacer una cuenta regresiba 
     
